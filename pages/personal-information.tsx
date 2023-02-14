4054dee4036049503fc858bbc2bcca96ebaf55fe
Brandon Rainey <brandonhrainey@gmail.com>
2023-02-13 23:56:27 -0500

    first commit

---
+++ pages/personal-information.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+export default function personalInformation() {
+  return (
+    <section>
+      <Header />
+      Hello
+    </section>
+  )
+}
